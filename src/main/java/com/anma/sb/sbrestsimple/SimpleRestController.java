6ec9b7c00765047dfaad3bbdb9b08d115157553d
Andrii Maliuta <quadr988@gmail.com>
2021-12-19 13:37:39 +0200

    metrics

--- src/main/java/com/anma/sb/sbrestsimple/SimpleRestController.java
+++ src/main/java/com/anma/sb/sbrestsimple/SimpleRestController.java
@@ -1,5 +1,7 @@
 package com.anma.sb.sbrestsimple;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,19 +10,22 @@ import java.util.List;
 @RestController
 public class SimpleRestController {
 
+    private Logger logger = LoggerFactory.getLogger(SimpleRestController.class);
     static List<Cat> cats = List.of(new Cat("Murz", 5), new Cat("Murko", 7));
+
 
     @GetMapping("/")
     public String home() {
+        logger.info("/home page opened");
         return "TEST message";
     }
 
     @GetMapping("/cats")
     public List<Cat> cats() {
+        logger.info("/cats page opened");
         return List.of(new Cat("Murz", 5), new Cat("Murko", 7));
     }
 }
-
 
 class Cat {
     private String name;
